cc5f4e0e5404de8d7b1ec9899d15141dd8dce2f3
Claus Strasburger <claus@strasburger.de>
2017-04-19 01:07:16 +0200

    add messages to validators

--- LdapForm.py
+++ LdapForm.py
@@ -3,8 +3,12 @@ from wtforms.fields.html5 import EmailField
 
 
 class LdapForm(Form):
-    uid = StringField('Username', validators=[validators.Length(min=4, max=25), validators.DataRequired()])
-    sammyNick = StringField('SammyNick', validators=[validators.Length(min=4, max=25), validators.DataRequired()])
+    uid = StringField('Username', validators=[
+        validators.Length(min=4, max=25, message="Username length is baad. min: 4, max: 25"),
+        validators.DataRequired(message="Username is required")])
+    sammyNick = StringField('SammyNick', validators=[
+        validators.Length(min=4, max=25, message="BananaNick is baad. min: 4, max: 25"),
+        validators.DataRequired(message="BanaNick is required!")])
 
     oldPassword = PasswordField('Old Password')
     password = PasswordField('New Password', [
@@ -12,7 +16,7 @@ class LdapForm(Form):
     ])
     confirm = PasswordField('Repeat Password')
 
-    mail = EmailField("Email", validators=[validators.Email()])
+    mail = EmailField("Email", validators=[validators.Email(message="Email is invalid!")])
 
     sshKey1 = StringField('SShPublicKey')
     sshKey2 = StringField('Alt. SSHPublicKey')
