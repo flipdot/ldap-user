40a88231adb24e35b5ce1f33073eddb84d906f0d
malled2002 <malte@drabesch.de>
2017-04-18 23:11:32 +0200

    add example config

---
+++ config-example.py
@@ -0,0 +1,7 @@
+SECRET = 'geheim'
+LDAP_HOST = 'ldaps://ldap.flipdot.org:636/'
+LDAP_USER_DN = "cn={:s},ou=members,dc=flipdot,dc=org"
+LDAP_ADMIN_DN = "cn=admin,dc=flipdot,dc=org"
+LDAP_ADMIN_PW = "password"
+
+LDAP_MEMBER_GID = "10000"
