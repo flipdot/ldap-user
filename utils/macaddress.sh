453a34fef21eeb9f77beebdf47f3040f22eb0d0f
malled2002 <malte@drabesch.de>
2017-07-03 01:12:12 +0200

    convert ldap mac fields to sed script

---
+++ utils/macaddress.sh
@@ -0,0 +1,26 @@
+SED_FILE=/tmp/ldap_macs.sed
+rm -f $SED_FILE
+macs=()
+IFS_BACKUP=$IFS
+IFS=$'\n'
+for x in `ldapsearch -h rail.fd -D cn=admin,dc=flipdot,dc=org -b ou=members,dc=flipdot,dc=org -w password sn macAddress | grep -v "^#"`; do
+        IFS=$IFS_BACKUP
+        if [[ $x =~ ^sn:\ .*$ ]] ; then
+                MACUSER=`echo -n "$x" | sed -rn 's/^sn: (.*)$/\1/p'`
+        fi
+        if [[ $x =~ ^sn::\ .*$ ]] ; then
+                MACUSER=`echo -n "$x" | sed -rn 's/^sn:: (.*)$/\1/p' | base64 -d`
+        fi
+        if [[ $x =~ ^macAddress:.*$ ]] ; then
+                MAC=`echo -n "$x" | sed -rn 's/^macAddress: ([0-9A-Fa-f:]*)$/\1/p'`
+                macs+=($MAC)
+        fi
+        if [[ $x =~ ^dn:\ cn=.*$ || $x =~ ^search.*$ ]]; then
+                echo "newline"
+                for m in ${macs[@]}; do
+                        echo "s/$m/$MACUSER/" >> $SED_FILE
+                done
+                macs=()
+                unset MACUSER
+        fi
+done
